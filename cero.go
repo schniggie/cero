8f06eb26c47400186f0a31a79363a09947d9d753
cd <root@schniggie.de>
2024-07-22 00:15:05 +0200

    Added CORS

--- cero.go
+++ cero.go
@@ -136,7 +136,7 @@ func main() {
 
 		log.Printf("Metrics server listening on port %d", port)
 
-		http.HandleFunc("/metrics", metricsHandler)
+		http.HandleFunc("/metrics", corsMiddleware(metricsHandler))
 		log.Fatal(http.Serve(listener, nil))
 	}()
 
@@ -403,4 +403,22 @@ func printMetrics() {
 // isPortInUseError checks if the error is due to the port being already in use
 func isPortInUseError(err error) bool {
 	return strings.Contains(err.Error(), "address already in use")
+}
+
+func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Handle preflight requests
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Call the next handler
+		next.ServeHTTP(w, r)
+	}
 }
